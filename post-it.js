a50f78d3bc003c58dab8c74c7c6e452d23f61968
ericchen0121 <eric.chen0121@gmail.com>
2013-08-13 12:33:35 -0700

    Z coding up javascript like a fiend

--- post-it.js
+++ post-it.js
@@ -1,61 +1,76 @@
-var Board = function( selector ) {
-  // Your board related code goes here
-  
-  // Use $elem to access the DOM element for this board
-  var $elem = $( selector );
-  
-  function initialize() {
-    // What needs to happen when this object is created?
-
-    // $('.post-it').on('click', function(e) {
-    //   e.stopPropagation();
-    // });
-
-    $elem.click(function(e) {
-      console.log("clicked in body");
-      postit = new PostIt(); // x-y coordinates
-    });
-
-  };
-
-  initialize();
-};
-
-var PostIt = function() {
-  console.log("creating a post it");
-  // Your post-it related code goes here
-  var postit_skeleton = "<div class='post-it' contenteditable='true'>\
-                           <div class='header' contenteditable='true'><a href='#'>X</a></div>\
-                            <div class='content' contenteditable='true'>\
-                         </div></div>"
-
-  function render() {
-    console.log('inside render');
-    $('#board').append(postit_skeleton);
-    var $new_postit = $('.post-it:last-child');
-    $new_postit.position({
-      my: "left top",
-      at: "left top",
-      of: event
-    });
-    $new_postit.draggable({ handle: ".header"})
-    $new_postit.on('click', function(e) {
-      e.stopPropagation();
-    });
-
-    var $new_postit_delete = $('.post-it:last-child a');
-    $new_postit_delete.on('click', function(e){
-      e.stopPropagation();
-      $(this).parent().parent().remove();
-    });
-  };
-
-  render();
-
-};
-
-$(function() {
-  // This code will run when the DOM has finished loading
-  board = new Board('#board');
+$(document).ready(function() {
+  board = new Board({ selector: '#board'});
 });
+function StickyView(sticky) {
+  this.element = $(this.template());
+  this.element.draggable();
+  this.element.position({
+    my: "top left",
+    at: "top left",
+    of: sticky.location
+  });
+  var content = this.element.find('.content');
+  content.text(sticky.content);
 
+  this.element.on('click', '.header a', function(e) {
+    sticky.remove();
+  });
+
+  var element = this.element;
+  $(sticky).on('removed', function() {
+    element.remove();
+  });
+
+  $(sticky).on('changed', function() {
+    content.text(sticky.content)
+  });
+  content.on('click', function(e) {
+    sticky.change(window.prompt());
+    return false;
+  })
+}
+
+StickyView.prototype.template = function() {
+  return $.trim($('#sticky-template').html());
+}
+
+function Sticky(options) {
+  this.content = options.content;
+  this.location = options.location;
+}
+
+Sticky.prototype.remove = function() {
+  $(this).trigger('removed');
+}
+Sticky.prototype.change = function(content) {
+  this.content = content;
+  $(this).trigger('changed');
+}
+
+function Board(options) {
+  function initialize(options) {
+ 
+    this.stickies = [];
+    this.selector = options.selector
+    this.element = $(this.selector);
+
+    var board = this;
+    this.element.on('click', function(e) { board.addSticky(e); });
+  }
+  
+
+
+  // We use call to coerce javascript to make sure "this" is the new board, instead of Window
+  initialize.call(this, options);
+}
+
+Board.prototype.addSticky = function(location) {
+
+  var sticky = new Sticky({ content: window.prompt(), location: location });
+  this.stickies.push(sticky);
+  this.element.append(new StickyView(sticky).element);
+  var board = this;
+  $(sticky).on('removed', function() {
+    board.stickies.delete(sticky);
+  });
+}
