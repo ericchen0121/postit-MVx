46dff2f3eb2624e17b2e6b8f33ab471bbfc63173
Cole Pilegard <c.pilegard@gmail.com>
2013-08-13 11:28:56 -0700

    can post and delete postits

--- post-it.js
+++ post-it.js
@@ -6,16 +6,56 @@ var Board = function( selector ) {
   
   function initialize() {
     // What needs to happen when this object is created?
+
+    // $('.post-it').on('click', function(e) {
+    //   e.stopPropagation();
+    // });
+
+    $elem.click(function(e) {
+      console.log("clicked in body");
+      postit = new PostIt(); // x-y coordinates
+    });
+
   };
 
   initialize();
 };
 
 var PostIt = function() {
+  console.log("creating a post it");
   // Your post-it related code goes here
+  var postit_skeleton = "<div class='post-it' contenteditable='true'>\
+                           <div class='header' contenteditable='true'><a href='#'>X</a></div>\
+                            <div class='content' contenteditable='true'>\
+                         </div></div>"
+
+  function render() {
+    console.log('inside render');
+    $('#board').append(postit_skeleton);
+    var $new_postit = $('.post-it:last-child');
+    $new_postit.position({
+      my: "left top",
+      at: "left top",
+      of: event
+    });
+    $new_postit.draggable({ handle: ".header"})
+    $new_postit.on('click', function(e) {
+      e.stopPropagation();
+    });
+
+    var $new_postit_delete = $('.post-it:last-child a');
+    $new_postit_delete.on('click', function(e){
+      e.stopPropagation();
+      $(this).parent().parent().remove();
+    });
+  };
+
+  render();
+
 };
 
 $(function() {
   // This code will run when the DOM has finished loading
-  Board.new('#board');
+  board = new Board('#board');
 });
+
